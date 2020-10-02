744792559cc241c48ca9e30fa0bc8020dae6061d
ryantimjohn <tim@alloy.us>
2020-10-02 09:54:59 -0400

    add hello world and update hamming

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,12 +1,12 @@
 local Hamming = {}
 
 function Hamming.compute(a, b)
-  h = 0
+  local h = 0
   if #a ~= #b then
     return -1
   else
     for i = 1, #a do
-      if string.sub(a, i, i) ~= string.sub(b, i, i) then
+      if a:sub(i, i) ~= b:sub(i, i) then
         h = h + 1
       end
     end
