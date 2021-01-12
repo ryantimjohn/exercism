5a131ad0cf299b8705b2c736521526d2e78ec509
ryantimjohn <tim@alloy.us>
2021-01-12 12:48:02 -0500

    update

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -2,20 +2,10 @@
 
 main () {
   o=""
-  if (( $1 % 3 == 0 )); then
-    o="${o}Pling"
-  fi
-  if (( $1 % 5 == 0 )); then
-    o="${o}Plang"
-  fi
-  if (( $1 % 7 == 0 )); then
-    o="${o}Plong"
-  fi
-  if (( ${#o} == 0 )); then
-    echo $1
-  else
-    echo $o
-  fi
+  (( $1 % 3 == 0 )) && o="${o}Pling"
+  (( $1 % 5 == 0 )) && o="${o}Plang"
+  (( $1 % 7 == 0 )) && o="${o}Plong"
+  (( ${#o} == 0 )) && echo $1 || echo $o
 }
 
 main "$@"
